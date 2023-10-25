6f80d920ff0128cbc68c306286513bc882651184
Sean Martin <martins7@tcd.ie>
2023-10-25 11:45:04 +0100

    Progress on compressed segmentation

--- examples/write_segmentation.py
+++ examples/write_segmentation.py
@@ -3,13 +3,14 @@ from ome_zarr.reader import Reader
 from pathlib import Path
 from math import ceil
 from tqdm import tqdm
-import logging
+import struct
 
 import numpy as np
 import dask.array as da
 
 
-def load_data(filename):
+def load_data(filename: Path) -> da.Array:
+    """Load the OME-Zarr data and return a dask array"""
     url = parse_url(filename)
     reader = Reader(url)
     nodes = list(reader())
@@ -22,17 +23,127 @@ def write_segmentation():
     pass
 
 
-def _get_grid_size_from_block_size(data_shape, block_size):
+def _get_grid_size_from_block_size(
+    data_shape: tuple[int, int, int], block_size: tuple[int, int, int]
+) -> tuple[int, int, int]:
+    """Calculate the grid size from the block size"""
     gx = ceil(data_shape[0] / block_size[0])
     gy = ceil(data_shape[1] / block_size[1])
     gz = ceil(data_shape[2] / block_size[2])
     return gx, gy, gz
 
 
-def _get_buffer_position(buffer):
+def _get_buffer_position(buffer: bytearray) -> int:
+    """Return the current position in the buffer"""
     if len(buffer) % 4 != 0:
         raise ValueError("Buffer length must be a multiple of 4")
     return len(buffer) // 4
+
+
+def _pad_block(block: da.Array, block_size: tuple[int, int, int]) -> da.Array:
+    """Pad the block to the given block size with zeros"""
+    return da.pad(
+        block,
+        (
+            (0, block_size[0] - block.shape[0]),
+            (0, block_size[1] - block.shape[1]),
+            (0, block_size[2] - block.shape[2]),
+        ),
+    )
+
+
+def _create_block_header(
+    buffer: bytearray,
+    lookup_table_offset: int,
+    encoded_bits: int,
+    encoded_values_offset: int,
+    block_offset: int,
+):
+    """
+    Create a block header (64-bit)
+
+    First 24 bits are the lookup table offset (little endian)
+    Next 8 bits are the number of bits used to encode the values
+    Last 32 bits are the offset to the encoded values (little endian)
+    All values are unsigned integers
+
+    Parameters
+    ----------
+    buffer : bytearray
+        The buffer to write the block header to
+    lookup_table_offset : int
+        The offset in the buffer to the lookup table for this block
+    encoded_bits : int
+        The number of bits used to encode the values
+    encoded_values_offset : int
+        The offset in the buffer to the encoded values for this block
+    block_offset : int
+        The offset in the buffer to the block header
+    """
+    struct.pack_into(
+        "<II",
+        buffer,
+        block_offset,
+        lookup_table_offset | (encoded_bits << 24),
+        encoded_values_offset,
+    )
+
+
+def _create_lookup_table(
+    buffer: bytearray, stored_lookup_tables: dict[bytes, int], unique_values: da.Array
+) -> tuple[int, int]:
+    """
+    Create a lookup table for the given values
+
+    Parameters
+    ----------
+    buffer : bytearray
+        The buffer to write the lookup table to
+    stored_lookup_tables : dict[bytes, int]
+        A dictionary mapping values to their offset in the buffer
+    unique_values : np.ndarray
+        The values to write to the buffer
+        Must be uint32 or uint64
+
+    Returns
+    -------
+    lookup_table_offset : int
+        The offset in the buffer to the lookup table for the given values
+    encoded_bits : int
+        The number of bits used to encode the values
+    """
+    unique_values = unique_values.astype(np.uint32).compute()
+    encoded_bits = int(np.ceil(np.log2(unique_values.shape[0])))
+    if encoded_bits > 32:
+        raise ValueError("Too many unique values in block")
+    values_in_bytes = unique_values.tobytes()
+    if values_in_bytes not in stored_lookup_tables:
+        lookup_table_offset = _get_buffer_position(buffer)
+        stored_lookup_tables[values_in_bytes] = lookup_table_offset
+        buffer.extend(values_in_bytes)
+    else:
+        lookup_table_offset = stored_lookup_tables[values_in_bytes]
+    return lookup_table_offset, encoded_bits
+
+
+def _create_encoded_values(buffer: bytearray, positions: da.Array) -> int:
+    """Create the encoded values for the given values
+
+    Parameters
+    ----------
+    buffer: bytearray
+        The buffer to write the encoded values to
+    values: da.Array
+        The values to encode
+
+    Returns
+    -------
+    encoded_values_offset: int
+        The offset in the buffer to the encoded values
+    """
+    encoded_values_offset = _get_buffer_position(buffer)
+    buffer.extend(positions.astype(np.uint32).compute().tobytes())
+    return encoded_values_offset
 
 
 def convert_to_segmentation(dask_data, block_size=(8, 8, 8)):
@@ -41,50 +152,27 @@ def convert_to_segmentation(dask_data, block_size=(8, 8, 8)):
     gx, gy, gz = _get_grid_size_from_block_size(dask_data.shape, block_size)
     stored_lookup_tables = {}
 
-    temp_uniques = []
-
     for x, y, z in tqdm(np.ndindex(gx, gy, gz), total=gx * gy * gz):
-        # Grab the data for this block
         block = dask_data[
             x * bx : (x + 1) * bx, y * by : (y + 1) * by, z * bz : (z + 1) * bz
         ]
-        # TODO pad the block to the block size
         unique_values, indices = da.unique(block, return_inverse=True)
-        unique_values = unique_values.astype(np.uint32).compute()
-        nbits = int(np.ceil(np.log2(unique_values.shape[0])))
-        if nbits > 32:
-            raise ValueError("Too many unique values in block")
-        values_in_bytes = unique_values.tobytes()
-        if values_in_bytes not in stored_lookup_tables:
-            offset = _get_buffer_position(buffer)
-            stored_lookup_tables[values_in_bytes] = offset
-            buffer.extend(values_in_bytes)
-        else:
-            offset = stored_lookup_tables[values_in_bytes]
+        block = _pad_block(block, block_size)
 
-        encoded_values_offset = _get_buffer_position(buffer)
-        # TODO implement encoding
-        buffer.extend(indices.astype(np.uint32).tobytes())
+        lookup_table_offset, encoded_bits = _create_lookup_table(
+            buffer, stored_lookup_tables, unique_values
+        )
+        encoded_values_offset = _create_encoded_values(buffer, indices)
+        block_offset = 8 * (x + gx * (y + gy * z))
+        _create_block_header(
+            buffer,
+            lookup_table_offset,
+            encoded_bits,
+            encoded_values_offset,
+            block_offset,
+        )
 
-        # Create the block header
-        # TODO improve but this is the basic idea
-        block_header = np.array([nbits, offset, encoded_values_offset], dtype=np.uint32)
-
-        # struct.pack_into("<II", buf, 8 * (x + gx * (y + gy * z)),
-        #  lookup_table_offset | (bits << 24),
-        #  encoded_values_offset)
-
-        # unique_values_bytes = unique_values.astype(np.uint32).tobytes()
-        # 1. block headers
-
-        # 2. block data as (ordered by x, y, z)
-        #   1. encoded values for the block
-        #   2. lookup table for the block
-
-    print(set(temp_uniques))
-    # 2. block data as (ordered by x, y, z)
-    #   1. encoded values for the block
-    #   2. lookup table for the block
+    return buffer
 
 
 def main(filenames):
