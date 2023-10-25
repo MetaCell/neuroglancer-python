51d0c7ef76448e7001d17c685b24753a870dfa82
Sean Martin <martins7@tcd.ie>
2023-10-25 13:25:10 +0100

    In progress with writing segmentation

--- examples/write_segmentation.py
+++ examples/write_segmentation.py
@@ -1,26 +1,78 @@
+from typing import Any
 from ome_zarr.io import parse_url
 from ome_zarr.reader import Reader
 from pathlib import Path
 from math import ceil
 from tqdm import tqdm
+import json
 import struct
+from dataclasses import dataclass
 
 import numpy as np
 import dask.array as da
 
+@dataclass
+class Chunk:
+    buffer: bytearray
+    dimensions: list[int]
+    
+    def get_name(self):
+        x_begin, x_end = self.dimensions[0], self.dimensions[1]
+        y_begin, y_end = self.dimensions[2], self.dimensions[3]
+        z_begin, z_end = self.dimensions[4], self.dimensions[5]
+        return f"{x_begin}-{x_end}_{y_begin}-{y_end}_{z_begin}-{z_end}"
+        
 
-def load_data(filename: Path) -> da.Array:
+def load_data(input_filepath: Path) -> da.Array:
     """Load the OME-Zarr data and return a dask array"""
-    url = parse_url(filename)
+    url = parse_url(input_filepath)
     reader = Reader(url)
     nodes = list(reader())
     image_node = nodes[0]
     dask_data = image_node.data[0]
-    return dask_data
+    return dask_data.persist()
 
 
-def write_segmentation():
-    pass
+def _create_metadata(
+    chunk_size: tuple[int, int, int],
+    block_size: tuple[int, int, int],
+    data_size: tuple[int, int, int],
+):
+    metadata = {
+        "@type": "neuroglancer_multiscale_volume",
+        "data_type": "uint32",
+        "num_channels": 1,
+        "scales": [
+            {
+                "chunk_sizes": [list(chunk_size)],
+                "encoding": "compressed_segmentation",
+                "compressed_segmentation_block_size": list(block_size),
+                "resolution": [1, 1, 1],
+                "key": "segmentation",
+                "size": list(data_size),
+            }
+        ],
+        "type": "segmentation",
+    }
+    return metadata
+
+
+def write_segmentation(
+    input_data: list[Chunk], metadata: dict[str, Any], output_directory: Path
+):
+    """Write the segmentation to the given directory"""
+    output_directory.mkdir(parents=True, exist_ok=True)
+    metadata_path = output_directory / "info"
+    output_chunk_directory = output_directory / "segmentation"
+
+    with open(metadata_path, "w") as f:
+        json.dump(metadata, f, indent=4)
+
+    for chunk in input_data:
+        output_filename = chunk.get_name()
+        output_filepath = output_chunk_directory / output_filename
+        with open(output_filepath, "wb") as f:
+            f.write(input_data)
 
 
 def _get_grid_size_from_block_size(
@@ -146,7 +198,9 @@ def _create_encoded_values(buffer: bytearray, positions: da.Array) -> int:
     return encoded_values_offset
 
 
-def convert_to_segmentation(dask_data, block_size=(8, 8, 8)):
+def _create_segmentation_chunk(dask_data, block_size=(8, 8, 8)):
+    print(dask_data.shape)
+    return 1
     buffer = bytearray()
     bx, by, bz = block_size
     gx, gy, gz = _get_grid_size_from_block_size(dask_data.shape, block_size)
@@ -172,18 +226,24 @@ def convert_to_segmentation(dask_data, block_size=(8, 8, 8)):
             block_offset,
         )
 
-    return buffer
+    # start_x, end_x = 
+    return Chunk(buffer, [])
 
+def create_segmentation(dask_data: da.Array, block_size):
+    for block in dask_data.blocks:
+        yield _create_segmentation_chunk(block, block_size)
 
-def main(filenames):
-    dask_data_actin = load_data(filenames[0])
-    dask_data_microtubules = load_data(filenames[1])
-    dask_data = [dask_data_actin, dask_data_microtubules]
-    convert_to_segmentation(dask_data[0], (128, 128, 128))
+def main(filename, block_size=(128, 128, 128)):
+    dask_data = load_data(filename)
+    metadata = _create_metadata(dask_data.chunksize, block_size, dask_data.shape)
+    print(dask_data.chunks)
+    chunks = [c for c in create_segmentation(dask_data, block_size)]
+    write_segmentation(chunks, metadata, filename.parent / "segmentation")
 
 
 if __name__ == "__main__":
     base_directory = Path("/media/starfish/LargeSSD/data/cryoET/data/segmentation")
     actin_filename = base_directory / "00004_actin_ground_truth_zarr"
     microtubules_filename = base_directory / "00004_MT_ground_truth_zarr"
-    main((actin_filename, microtubules_filename))
+    main(actin_filename)
+    main(microtubules_filename)
