563c226fb0308c24fe359df6fe5f8a0979b81865
Sean Martin <martins7@tcd.ie>
2024-03-19 11:45:10 +0100

    refactor: rename local volume file

--- examples/local_volume_screenshot.py
+++ examples/local_volume_screenshot.py
@@ -34,29 +34,23 @@ from neuroglancer_utils.layer_utils import add_render_panel
 def add_image_layer(state, **kwargs):
     shape = (2500, 2500, 250)
     data = zarr.full(shape=shape, fill_value=255, chunks=[157, 157, 32], dtype="uint8")
-    print(data.info)
     dimensions = neuroglancer.CoordinateSpace(
         names=["x", "y", "z"], units="nm", scales=[40, 40, 400]
     )
     local_volume = neuroglancer.LocalVolume(data, dimensions)
     state.layers["image"] = neuroglancer.ImageLayer(
         source=local_volume,
-        volume_rendering=True,
-        tool_bindings={
-            "A": neuroglancer.VolumeRenderingGainTool(),
-        },
-        panels=[add_render_panel()],
+        volume_rendering_mode="ON",
         **kwargs,
     )
 
 def get_shader():
     return """
-#uicontrol float gain slider(min=0, max=10, default=1.0)
 #uicontrol invlerp normalized(range=[0,255], clamp=true)
 #uicontrol vec3 color color(default="white")
 void main() {
     float val = normalized();
-    emitRGBA(vec4(color, val * gain));
+    emitRGBA(vec4(color, val));
     }
     """
 
@@ -64,12 +58,25 @@ if __name__ == "__main__":
     viewer = launch_nglancer()
     with viewer.txn() as s:
         add_image_layer(s, shader=get_shader())
-    threedee_view(viewer)
+    #threedee_view(viewer)
     remove_axis_lines(viewer)
-    show_statistics(viewer)
     update_title(viewer, "Volume control example")
     set_gpu_memory(viewer, gpu_memory=2)
     update_projection(viewer, orientation=[0.25, 0.6, 0.65, 0.3])
     open_browser(viewer, hang=False)
     sleep(4) # TODO this is a hack to wait for viewer to open
     update_projection(viewer, scale=4555, depth=4429)
+
+    s1 = viewer.screenshot(size=[1000, 1000])
+    print(s1)
+
+    with viewer.txn() as s:
+        s.layers["image"].volume_rendering_gain = 1.0
+    s2 = viewer.screenshot(size=[1000, 1000])
+    print(s2)
+
+    with open("p1.png", "wb") as f:
+        f.write(s1.screenshot.image)
+
+    with open("p2.png", "wb") as f:
+        f.write(s2.screenshot.image)
