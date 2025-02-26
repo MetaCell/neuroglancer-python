a4506c284eb5c22d5d5c949b205177a4f27a5d21
Sean Martin <martins7@tcd.ie>
2025-02-26 11:09:41 +0100

    feat: support convert on nrrd

--- examples/load_nrrd.py
+++ examples/load_nrrd.py
@@ -4,13 +4,32 @@ import neuroglancer
 import neuroglancer.cli
 import nrrd
 from pathlib import Path
+from cloudvolume import CloudVolume
 
 HERE = Path(__file__).parent
-NRRD_FILE_PATH = HERE / "volume1.nrrd"
+
+NAMES = ["12vj", "1567", "101b"]
+PATHS = [HERE / f"{name}.nrrd" for name in NAMES]
+OUTPUT_PATHS = [f"file://datasets/{name}" for name in NAMES]
 
 
-def add_image_layer(state):
-    readdata, header = nrrd.read(NRRD_FILE_PATH)
+def convert_to_precomputed(nrrd_path, output_path):
+    readdata, header = nrrd.read(nrrd_path)
+    # Cloud volume expects resolution in nm - but given in um
+    scales = [header["space directions"][i][i] * 1000 for i in range(3)]
+    CloudVolume.from_numpy(
+        readdata,
+        vol_path=output_path,
+        chunk_size=(256, 256, 128),
+        resolution=scales,
+        layer_type="image",
+        progress=True,
+        compress=False,
+    )
+
+
+def add_image_layer(state, path, name="image"):
+    readdata, header = nrrd.read(path)
     scales = [header["space directions"][i][i] for i in range(3)]
     dimensions = neuroglancer.CoordinateSpace(
         names=["x", "y", "z"], units="um", scales=scales
@@ -18,7 +37,7 @@ def add_image_layer(state):
     data = readdata
     local_volume = neuroglancer.LocalVolume(data, dimensions)
     state.layers.append(
-        name="image",
+        name=name,
         layer=neuroglancer.ImageLayer(
             source=local_volume,
             volume_rendering_mode="ON",
@@ -47,7 +66,12 @@ def launch_nglancer():
 def main():
     viewer = launch_nglancer()
     with viewer.txn() as s:
-        add_image_layer(s)
+        for i in range(3):
+            convert_to_precomputed(PATHS[i], OUTPUT_PATHS[i])
+            path = PATHS[i]
+            name = NAMES[i]
+            add_image_layer(s, path, name)
+    exit(-1)
     webbrowser.open_new(viewer.get_viewer_url())
 
 
